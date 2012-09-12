1959d09b64d5f3478312248848ffcf03e566bf4f
mniip <mniip@yandex.ru>
2012-09-12 20:53:00 +0400

    Initial commit

---
+++ irc.lua
@@ -0,0 +1,458 @@
+-- ************************************************************************
+-- ** Copyright 2012 mniip
+-- **               mniip@yandex.ru
+-- ************************************************************************
+-- ** This program is free software: you can redistribute it and/or modify
+-- ** it under the terms of the GNU General Public License as published by
+-- ** the Free Software Foundation, either version 3 of the License, or
+-- ** (at your option) any later version.
+-- ** 
+-- ** This program is distributed in the hope that it will be useful,
+-- ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- ** GNU General Public License for more details.
+-- ** 
+-- ** You should have received a copy of the GNU General Public License
+-- ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-- ************************************************************************
+
+version="xsTPTIRC v1.0 beta by mniip"
+
+local n=tpt.get_name()
+if not n or n=="" then
+ error"you MUST be logged in to use xsTPTIrc"
+end
+local mynick=n
+
+local conf={}
+
+local function loadpresets()
+ local f=io.open"irc.conf"
+ if not f then
+  f=io.open("irc.conf","w")
+  f:write('nick="'..mynick..'"\nserver="irc.freenode.net"\nport=6667\nautojoin={"#powder-social"}')
+  f:close()
+  f=io.open"irc.conf"
+ end
+ local s=f:read"*a"
+ f:close()
+ f=loadstring(s)
+ if f then
+  setfenv(f,conf)
+  f()
+ end
+ mynick=tostring(conf.nick) or n
+ conf.server=tostring(conf.server) or "irc.freenode.net"
+ conf.port=tonumber(conf.port) or 6667
+ conf.autojoin=type(conf.autojoin)=="table" and conf.autojoin or {"#powder"}
+end
+
+loadpresets()
+
+local succ,socket=pcall(require,"socket")
+if not succ then
+ error"could not find luasocket"
+end
+
+local drawtext=function(...)return pcall(tpt.drawtext,...)end
+
+local tabs
+local ctab=1
+local edit=""
+local cpos=0
+
+local wx,wy=10,10
+local wcolor={255,255,0}
+local tabtextcolor={255,255,255}
+local ctabcolor={255,0,255,63}
+local nickcolor={0,255,255}
+local textcolor={127,255,255}
+
+local function ui()
+ tpt.fillrect(wx,wy,600,360,0,0,0,240)
+ -- Window
+tpt.drawrect(wx,wy,600,360,unpack(wcolor))
+ -- Topic
+ tpt.drawrect(wx,wy+12,484,12,unpack(wcolor))
+ local f=#tabs[ctab].topic
+ while tpt.textwidth(tabs[ctab].topic:sub(1,f))>480 do
+  f=f-1
+ end
+ drawtext(wx+2,wy+14,tabs[ctab].topic:sub(1,f),unpack(wcolor))
+ tpt.drawrect(wx+484,wy+12,12,12,unpack(wcolor))
+ -- Tabs
+ if ctab>#tabs then
+  ctab=#tabs
+ end
+ local ox=0
+ for i=1,#tabs do
+  tpt.drawrect(wx+ox,wy,#tabs[i].name*6+2,12,unpack(wcolor))
+  drawtext(wx+ox+2,wy+2,tabs[i].name,unpack(tabtextcolor))
+  if i==ctab then 
+   tpt.fillrect(wx+ox,wy,#tabs[i].name*6+3,12,unpack(ctabcolor))
+  end
+  ox=ox+#tabs[i].name*6+2
+ end
+ -- Nicklist
+ tpt.drawrect(wx+496,wy+12,104,336,unpack(wcolor))
+ for i,v in ipairs(tabs[ctab].nicks) do
+  if i<=33 then
+   drawtext(wx+498,wy+4+10*i,v,unpack(nickcolor))
+  end
+ end
+ tpt.drawrect(wx,wy+24,496,324,unpack(wcolor))
+ tpt.drawrect(wx+587,wy+348,13,12,unpack(wcolor))
+ drawtext(wx+589,wy+351,">>",unpack(wcolor))
+ -- Text
+ for i=1,math.min(#tabs[ctab].text,32) do
+  drawtext(wx+2,wy+348-i*10,tabs[ctab].text[i],unpack(textcolor))
+ end
+ drawtext(wx+2,wy+351,edit,unpack(textcolor))
+end
+
+local function isinrect(x,y,xz,yz,w,h)
+ return x>=xz and y>=yz and x<xz+w and y<yz+h
+end
+
+local function wprint(n,s)
+ while #s>0 do
+  local f=#s
+  while tpt.textwidth(s:sub(1,f))>493 do
+   f=f-1
+  end
+  table.insert(tabs[n].text,1,s:sub(1,f))
+  s=s:sub(f+1)
+ end
+end
+
+local function findtab(s)
+ for i,v in ipairs(tabs) do
+  if s:lower()==v.name:lower() then
+   return i
+  end
+ end
+end
+
+local function send()
+ if edit:match"^/" then
+  local command,params=edit:match"/(%S+)%s*(.*)"
+  if command:lower()=="clear" then
+   if params~="" then
+    (tabs[findtab(params)or tonumber(params)or -1]or {}).text={}
+   else
+     tabs[ctab].text={}
+   end
+  elseif command:lower()=="close" then
+   if params~="" then
+    table.remove(tabs,findtab(params)or tonumber(params) or 0)
+   else
+    table.remove(tabs,ctab)
+   end
+  elseif command:lower()=="open" then
+   table.insert(tabs,{name=params,nicks={},topic="",text={}})
+  elseif command:lower()=="ctcp" then
+   local who,what=params:match"(%S+)%s*(.*)"
+   c:send("PRIVMSG "..who.." :\1"..what.."\1\n")
+  elseif command:lower()=="me" then
+   c:send("PRIVMSG "..tabs[ctab].name.." :\1ACTION "..params.."\1\n")
+   wprint(ctab,"* "..mynick.." "..params)
+  else
+   c:send(edit:match"/(.*)".."\n")
+   wprint(ctab,">"..edit)
+  end
+ else
+  c:send("PRIVMSG "..tabs[ctab].name.." :"..edit.."\n")
+  wprint(ctab,"<"..mynick.."> "..edit)
+ end
+ edit=""
+end
+
+local function mouse(x,y,b,n)
+ if n==1 then
+  -- Tabs
+  local ox=0
+  for i=1,#tabs do
+   if isinrect(x,y,wx+ox,wy,#tabs[i].name*6+3,12) then
+    ctab=i
+   end
+   ox=ox+#tabs[i].name*6+2
+  end
+  if isinrect(x,y,wx+587,wy+348,13,12) then
+   send()
+  end
+ end
+ return false
+end
+
+local function resortnicks(n)
+ local d=tabs[n].nicks
+ local a,b={},{}
+ for _,v in ipairs(d) do
+  a[#a+1],b[#b+1]=v:match"([@+]?)(.*)"
+ end
+ local op,vc,rg={},{},{}
+ for i,v in ipairs(a) do
+  if v=="@" then
+   table.insert(op,b[i])
+  elseif v=="+" then
+   table.insert(vc,b[i])
+  else
+   table.insert(rg,b[i])
+  end
+ end
+ local function f(a,b)return a:lower()<b:lower() end
+ table.sort(op,f)
+ table.sort(vc,f)
+ table.sort(rg,f)
+ for i,_ in ipairs(d) do
+  d[i]=nil
+ end
+ for _,v in ipairs(op) do
+  table.insert(d,"@"..v)
+ end
+ for _,v in ipairs(vc) do
+  table.insert(d,"+"..v)
+ end
+ for _,v in ipairs(rg) do
+  table.insert(d,v)
+ end
+end
+
+local function receive()
+ local s=c:receive"*l"
+ if s then
+  s=s:gsub("\n","")
+  local sender,command,params=s:match":(%S+) (%S+)%s*(.*)"
+  if not sender then
+   command,params=s:match"(%S+)%s*(.*)"
+  end
+  if s:match"(.-)NOTICE %* :%*%*%* Checking Ident" then
+   c:send("NICK "..mynick.."\n")
+   c:send("USER xstptirc _ _ :TPT login:"..n.."\n")
+  elseif command:lower()=="join" then
+   local channel=params:match"(%S+)"
+   local nick=sender:match"([^!]+)"
+   if nick:lower()==mynick:lower() then
+    table.insert(tabs,{name=channel,nicks={},topic="",text={}})
+    ctab=#tabs
+    wprint(1,"*** You have joined "..channel)
+   else
+    table.insert(tabs[findtab(channel)or 1].nicks,nick)
+   end
+   wprint(findtab(channel)or 1,"*** "..nick.." has joined "..channel)
+   resortnicks(findtab(channel)or 1)
+  elseif command=="376" then
+   for _,v in ipairs(conf.autojoin) do
+    c:send("JOIN "..v.."\n")
+   end
+  elseif command:lower()=="privmsg" then
+   local channel,text=params:match"(%S+) :(.*)"
+   local nick=sender:match"([^!]+)"
+   if text:find"\1" then
+    local command,params=text:match"\1(%S+)%s*(.*)\1"
+    if command:lower()=="ping" then
+     c:send("NOTICE "..nick.." :\1PING "..params.."\1\n")
+    elseif command:lower()=="time" then
+     c:send("NOTICE "..nick.." :\1TIME "..os.date"%c".."\1\n")
+    elseif command:lower()=="version" then
+     c:send("NOTICE "..nick.." :\1VERSION "..version.."\1\n")
+    elseif command:lower()=="action" then
+    if channel:lower()==mynick:lower() then
+     if not findtab(nick) then
+      table.insert(tabs,{name=nick,nicks={},topic="Query with "..nick,text={}})
+     end
+     channel=nick
+    end
+    wprint(findtab(channel)or 1,"* "..nick.." "..params)
+    end
+   else
+    if channel:lower()==mynick:lower() then
+     if not findtab(nick) then
+      table.insert(tabs,{name=nick,nicks={},topic="Query with "..nick,text={}})
+     end
+     channel=nick
+    end
+    wprint(findtab(channel)or 1,"<"..nick.."> "..text)
+   end
+  elseif command=="433" then
+   mynick=mynick.."-"
+   c:send("NICK "..mynick.."\n")
+  elseif command=="332" then
+   local channel,topic=params:match"%S+ (%S+) :(.*)"
+   tabs[findtab(channel)or 1].topic=topic
+   wprint(findtab(channel)or 1,"*** Topic for "..channel.." is: ")
+   wprint(findtab(channel)or 1,topic)
+  elseif command=="333" then
+   local channel,whom,when=params:match"%S+ (%S+) (%S+) (%S+)"
+   wprint(findtab(channel)or 1,"*** Topic set by "..whom.." at "..os.date("%c",when))
+  elseif command=="353" then
+   local channel,nicks=params:match"%S+ = (%S+) :(.*)"
+   for nick in nicks:gmatch"(%S+)" do
+    table.insert(tabs[findtab(channel)or 0].nicks,nick)
+   end
+   resortnicks(findtab(channel)or 0)
+  elseif command=="366" then
+  elseif command:lower()=="ping" then
+   c:send("PONG\n")
+  elseif command:lower()=="part" then
+   local channel=params:match"(%S+)"
+   local nick=sender:match"([^!]+)"
+   for i,v in ipairs(tabs[findtab(channel)or 0].nicks) do
+    if v:lower():gsub("[@+]","")==nick:lower() then
+     table.remove(tabs[findtab(channel)or 1].nicks,i)
+     break
+    end
+   end
+   wprint(findtab(channel)or 1,"*** "..nick.." has left "..channel)
+   if nick:lower()==mynick:lower() then
+    table.remove(tabs,findtab(channel)or -1)
+    wprint(1,"*** You have left "..channel)
+   end
+  elseif command:lower()=="quit" then
+   local nick=sender:match"([^!]+)"
+   local reason=params:match"%s*:(.*)"
+   for i,v in ipairs(tabs) do
+    for j,n in ipairs(v.nicks) do
+     if n:lower():gsub("[@+]","")==nick:lower() then
+      wprint(i,"*** "..nick.." has quit ("..reason..")")
+      table.remove(v.nicks,j)
+      break
+     end
+    end
+   end
+  elseif command:lower()=="mode" then
+   local nick=sender:match"([^!]+)"
+   local whom,modes=params:match"(%S+) (.*)"
+   wprint(findtab(whom)or 1,"*** "..nick.." sets modes for "..whom.." : "..modes)
+   if whom:match"#" then
+    tabs[findtab(whom)or 1].nicks={}
+    c:send("NAMES "..whom.."\n")
+   end
+  elseif command:lower()=="nick" then
+   local nick=sender:match"([^!]+)"
+   local newnick=params:match":(%S+)"
+   for i,v in ipairs(tabs) do
+    for j,n in ipairs(v.nicks) do
+     if n:lower():gsub("[@+]","")==nick:lower() then
+      wprint(i,"*** "..nick.." has changed nick to "..newnick)
+      v.nicks[j]=n:gsub("[^@+]","")..newnick
+      break
+     end
+    end
+   end
+   if nick:lower()==mynick:lower() then
+    mynick=newnick
+   end
+  elseif command:lower()=="notice" then
+   local nick=sender:match"([^!]+)"
+   local text=params:match"%S+ :(.*)"
+   wprint(ctab,"-- "..nick.." -- "..text)
+  else
+   wprint(1,s)
+  end
+ end
+end
+
+local function caps(a)
+ local from="`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./"
+ local to=  "~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?"
+ local s,e=from:find(a,1,true)
+ return to:sub(s,e) or a
+end
+
+local function key(a,b,c,d)
+ if d==1 then
+  if b>31 and b<127 then
+   edit=edit..(c%2==0 and string.char(b) or caps(string.char(b)))
+  elseif b==13 then
+   send()
+  elseif b==8 then
+   edit=edit:sub(1,#edit-1)
+  elseif b==9 then
+   local txt,lw=edit:match"(.*%s)(%S+)"
+   lw=lw or edit
+   txt=txt or ""
+   for _,v in ipairs(tabs[ctab].nicks) do
+    if v:gsub("[@+]",""):sub(1,#lw):lower()==lw:lower() then
+     edit=txt..v:gsub("[@+]","")
+    end
+   end
+  else
+   return true
+  end
+ end
+ return false
+end
+
+local launched=false
+local visible=false
+
+local function appear()
+ if not visible then
+  tpt.register_step(ui)
+  tpt.register_mouseclick(mouse)
+  tpt.register_keypress(key)
+ end
+ visible=true
+end
+
+local function disappear()
+ if visible then
+  tpt.unregister_step(ui)
+  tpt.unregister_mouseclick(mouse)
+  tpt.unregister_keypress(key)
+ end
+ visible=false
+end
+
+local function start()
+ if not launched then
+  tabs={{name="-server-",
+  nicks={},
+  topic=version,
+  text={"--- "..version.." ---","The idea becomes real","Line by line"}}}
+  ctab=1
+  edit=""
+  cpos=0
+  c=socket.tcp()
+  c:connect(conf.server,conf.port)
+  c:settimeout(0)
+  tpt.register_step(receive)
+ end
+ launched=true
+end
+
+local function stop()
+ if launched then
+  c:send"QUIT\n"
+  c:close()
+  c=nil
+  tpt.unregister_step(receive)
+ end
+ disappear()
+ launched=false
+end
+
+
+local function keyhandler(a,b,c,d)
+ if d==1 and math.floor(c/64)%2==1 then
+  if a=="m" then
+   if c%2==0 then
+    if visible then
+     disappear()
+    else
+     if not launched then
+      start()
+     end
+     appear()
+    end
+   else
+    disappear()
+    stop()
+   end
+  end
+  return false
+ end
+end
+
+tpt.register_keypress(keyhandler)
